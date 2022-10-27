e0c7aea0169b9c9e66526271f8d55e73d9c91a55
FelipeHardmann <fashardmann@gmail.com>
2022-10-27 11:51:27 -0300

    Add arquivos heranca

--- programacao_orientada_objetos/heranca_multipla/classes.py
+++ programacao_orientada_objetos/heranca_multipla/classes.py
@@ -1,4 +1,4 @@
-class A:
+"""class A:
     def falar(self):
         print('Falando... Estou em A.')
 
@@ -13,10 +13,26 @@ class C(A):
         print('Falando... Estou em C.')
 
 # A classe D tem tudo que tem B e C
+# A classe começa a procurar o método da esquerda para direita
+# Então se existir métodos com o mesmo nome ele vai escolher o primeiro.
 class D(B, C):
     pass
 
 
 '''
 Um sistema pode ficar muito complexo se tiver muita herança
-'''
+'''"""
+
+from smartphone import Smartphone
+
+
+smartphone = Smartphone('Iphone')
+smartphone.conectar()
+smartphone.desligar()
+smartphone.ligar()
+smartphone.conectar()
+smartphone.conectar()
+smartphone.conectar()
+smartphone.desligar()
+smartphone.conectar()
+smartphone.desconectar()
---
+++ programacao_orientada_objetos/heranca_multipla/smartphone.py
@@ -0,0 +1,39 @@
+from eletronico import Eletronico
+from log import LogMixin
+
+
+class Smartphone(Eletronico, LogMixin):
+    def __init__(self, nome):
+        super().__init__(nome)
+        self._conectado = False
+
+    def conectar(self):
+        if not self._ligado:
+            info = f'{self._nome} não está ligado'
+            print(info)
+            self.log_error(info)
+            return
+        
+        if self._conectado:
+            error = f'{self._nome} já está conectado'
+            print(error)
+            self.log_error(error)
+            return
+
+        info = f'{self._nome} está conectado'
+        print(info)
+        self.log_info(info)
+        self._conectado = True
+
+    def desconectar(self):
+        if not self._conectado:
+            error = f'{self._nome} não está conectado' 
+            print(error)
+            self.log_error(error)
+            return
+        
+        info = f'{self._nome} foi desligado'
+        print(info)
+        self.log_info(info)
+        self._ligado = False
+
