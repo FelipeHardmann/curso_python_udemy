eef3f03b2e4d95ff9d553bbb54a25e430d7d3105
FelipeHardmann <fashardmann@gmail.com>
2023-02-26 22:40:06 -0300

    Add exceptions add notes

--- programacao_orientada_objetos/exceptions/aula146.py
+++ programacao_orientada_objetos/exceptions/aula146.py
@@ -9,7 +9,8 @@ class OtherMyError(Exception):
 
 
 def levantar():
-    exception_ = MyError('a', 'b', 'c') 
+    exception_ = MyError('a', 'b', 'c')
+    # exception_.add_note('Olha a nota 1')  Só acontece no Python 11
     raise exception_
 
 try:
@@ -18,4 +19,6 @@ except (MyError, ZeroDivisionError) as error:
     print(error.__class__.__name__)
     print(error)
     exception_ = OtherMyError('Vou lançar dnv') 
+    # exception_.__notes__ += error.__notes__.copy() Só acontece no Python 11
+    # Add notas de error em uma nota
     raise exception_ from error
