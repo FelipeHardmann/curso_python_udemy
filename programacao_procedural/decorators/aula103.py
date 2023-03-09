11235567f6a3a812dc96731c754e97e266d45119
FelipeHardmann <fashardmann@gmail.com>
2023-03-09 09:39:35 -0300

    Add procedural

---
+++ programacao_procedural/decorators/aula103.py
@@ -0,0 +1,24 @@
+def criar_funcao(func):
+    def interna(*args, **kwargs):
+        print('Vou te decorar')
+        for arg in args:
+            e_string(arg)
+        resultado = func(*args, **kwargs)
+        print(f'O seu resultado foi {resultado}.')
+        print('Ok, agora você foi decorada')
+        return resultado
+    return interna
+
+
+def inverte_string(string):
+    return string[::-1]
+
+
+def e_string(param):
+    if not isinstance(param, str):
+        raise TypeError('param deve ser uma string')
+
+
+inverte_string_checando_parametro = criar_funcao(inverte_string)
+invertida = inverte_string_checando_parametro('123')
+print(invertida)
