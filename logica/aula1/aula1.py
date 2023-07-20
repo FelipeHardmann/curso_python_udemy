06102df13da3860f0ce0aad4a1be3bc8600f23fe
FelipeHardmann <fashardmann@gmail.com>
2023-07-20 16:19:45 -0300

    aula.1

--- logica/aula1/aula1.py
+++ logica/aula1/aula1.py
@@ -1,4 +1,9 @@
-print('Olá mundo!')
+# print('Olá mundo!')
 #Aula de comentários
 '''Explicação sobre os tipos de comentários'''
-
+contador = 0
+while contador <= 10:
+  print(contador)
+  contador += 1
+else:
+  print(f'A contagem do {contador} acabou')
